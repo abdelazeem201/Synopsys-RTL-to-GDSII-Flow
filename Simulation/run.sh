e44a227bc87c3fac4732ac219ab2cada6cc49387
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2025-06-13 21:45:44 +0300

    run.sh

--- Simulation/run.sh
+++ Simulation/run.sh
@@ -5,12 +5,12 @@ dhm_path=../rtl/
 bench=$dhm_path/bench
 wave_dir=$dhm_path/sim/rtl_sim/i2c_verilog/waves
 
-vcs	-debug_all							\
+vcs	-debug_all						\
 	+incdir+$bench/verilog					\
 	+incdir+$dhm_path/rtl/verilog				\
 								\
 	$dhm_path/rtl/*.v\
-	$dhm_path/sim/*.v			\
+	$dhm_path/sim/*.v					\
 								\
 	-R
 	
